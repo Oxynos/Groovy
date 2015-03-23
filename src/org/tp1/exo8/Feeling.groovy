fe9ad9245e258ee53feaf41385a844a581cc9c81
Cédric Rohaut <rohaut@icloud.com>
2015-03-23 13:24:58 +0100

    Exo 8 - Test 2 - Multi Assignment : OK

--- src/org/tp1/exo8/Feeling.groovy
+++ src/org/tp1/exo8/Feeling.groovy
@@ -7,7 +7,34 @@ public enum Feeling {
     Happy, Sad, Neutral, Suicidal, Anticipation, Surprised, Relaxed, Guilty
 
     // ------------ START EDITING HERE ----------------------
+    boolean isCase(Cartoon c) {
+        switch (c.feeling) {
+            case Guilty:
+                return true
+            default:
+                return false
+        }
+    }
 
+    boolean isCase(Person p) {
+        boolean b = true
+        p.feelings.each { it ->
+            switch (it) {
+                case Anticipation:
+                    break
+                case Happy:
+                    break
+                case Sad:
+                    break
+                case Suicidal:
+                    break
+                default:
+                    b = false
+                    break
+            }
+        }
+        return true
+    }
     // ------------ STOP EDITING HERE  ----------------------
 
 
